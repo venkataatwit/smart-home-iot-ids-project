2dac0aa7de3b5cf6a0db0ec27ec3794c1ed21321
venkataatwit <116669533+venkataatwit@users.noreply.github.com>
2026-07-06 10:02:21 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
+from src.capture import start_capture
 
+def main():
+    print("Starting Smart Home IoT Intrusion Detection System...")
+    start_capture()
+
+if __name__ == "__main__":
+    main()
